acfe4c0da364d8ab3f98f5523ed0b10c27db929f
Tobin Schwaiger-Hastanan <tobins@gmail.com>
2019-02-07 13:59:37 -0800

    Initial Commit

---
+++ Argh!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Argh!
+//
+//  Created by Tobin Schwaiger-Hastanan on 2/7/19.
+//  Copyright © 2019 Tobin Schwaiger-Hastanan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Argh!Tests/Argh_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Argh_Tests.swift
+//  Argh!Tests
+//
+//  Created by Tobin Schwaiger-Hastanan on 2/7/19.
+//  Copyright © 2019 Tobin Schwaiger-Hastanan. All rights reserved.
+//
+
+import XCTest
+@testable import Argh_
+
+class Argh_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
